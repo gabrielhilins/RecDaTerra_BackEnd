eef9c2161c0992f6db40b02606f2e18c5abf0174
Arthur Vinicius De Sena Moraes <123411782+lngg21@users.noreply.github.com>
2024-05-15 09:21:10 -0300

    Update Notificacao.java

--- src/main/java/com/example/projetopoobackendequipe4/model/Notificacao.java
+++ src/main/java/com/example/projetopoobackendequipe4/model/Notificacao.java
@@ -1,4 +1,45 @@
 package com.example.projetopoobackendequipe4.model;
 
+
+//import
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+//import
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+
+  //Inicio da classe Notificação
 public class Notificacao {
+    //Id
+    @id
+    @GeneratedValue(Strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //Contéudo
+    @Column(length = 100, nullable = false)
+    private String conteudo;
+
+    //Usuario que a notificação chegará
+    @Column(length = 20, nullable = false)
+    private Usuario usarioDestino;
+
+    //Data e horario que a notificação chega
+    @Column(length= 15, nullable = false)
+    private LocalDateTime dataHora;
+
+    
+
+
+
 }
