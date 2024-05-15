a918d40abf7a65496fb14523e724ffeeb6fcad84
Ceciliasitcovsky <131723560+Ceciliasitcovsky@users.noreply.github.com>
2024-05-15 10:36:20 -0300

    fazendo modelo de produtor

--- src/main/java/com/example/projetopoobackendequipe4/model/Produto.java
+++ src/main/java/com/example/projetopoobackendequipe4/model/Produto.java
@@ -1,4 +1,49 @@
 package com.example.projetopoobackendequipe4.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "produtor_id", referencedColumnName = "id")
+    private Produtor produtor;
+
+    @Column(length = 20, nullable = false)
+    private String nomeProduto;
+
+    @Column(length = 100, nullable = true)
+    private String descricao;
+
+    @Column(length = 30, nullable = false)
+    private String categoria;
+
+    @Column(length = 10, nullable = true)
+    private int estoque;
+
+    @Column(nullable = false)
+    private boolean disponibilidade;
+
+    @Column(nullable = true)
+    private byte[] fotoProduto;
 }
