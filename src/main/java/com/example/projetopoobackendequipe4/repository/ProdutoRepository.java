e36ed063105923a809e58fe4182f2f6155b11e58
Ceciliasitcovsky <131723560+Ceciliasitcovsky@users.noreply.github.com>
2024-05-15 10:36:57 -0300

    fazendo produto repository

--- src/main/java/com/example/projetopoobackendequipe4/repository/ProdutoRepository.java
+++ src/main/java/com/example/projetopoobackendequipe4/repository/ProdutoRepository.java
@@ -1,4 +1,12 @@
 package com.example.projetopoobackendequipe4.repository;
 
-public class ProdutoRepository {
-}
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.projetopoobackendequipe4.model.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    
+} 
