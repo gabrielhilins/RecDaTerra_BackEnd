bcd0df83e413e3d1c3cf34bd341f4720eaa92679
Arthur Vinicius De Sena Moraes <123411782+lngg21@users.noreply.github.com>
2024-05-15 09:45:37 -0300

    Update NotificacaoRepository.java

--- src/main/java/com/example/projetopoobackendequipe4/repository/NotificacaoRepository.java
+++ src/main/java/com/example/projetopoobackendequipe4/repository/NotificacaoRepository.java
@@ -1,4 +1,17 @@
 package com.example.projetopoobackendequipe4.repository;
 
-public class NotificacaoRepository {
+import java.util.List;
+
+import org.springframewok.data.jpa.repository.JpaRepository;
+import org.springframewok.stereotype.Repository;
+
+import com.example.projetopoobackendequipe4.model.Notificacao;
+
+
+@Repository
+public interface NotificacaoRepository extends JpaRepository<Notificacao, Long>{
+
+    List<Notificacao> encontraPorConteudo(String conteudo);
+
+
 }
