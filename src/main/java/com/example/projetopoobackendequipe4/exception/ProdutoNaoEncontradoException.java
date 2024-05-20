930e17aa066a03e5c6ba202d52fff6258bbe3dc6
Ceciliasitcovsky <131723560+Ceciliasitcovsky@users.noreply.github.com>
2024-05-20 20:00:12 -0300

    Atualizando ProdutoNaoEncontradoException.java

--- src/main/java/com/example/projetopoobackendequipe4/exception/ProdutoNaoEncontradoException.java
+++ src/main/java/com/example/projetopoobackendequipe4/exception/ProdutoNaoEncontradoException.java
@@ -1,4 +1,7 @@
 package com.example.projetopoobackendequipe4.exception;
 
-public class ProdutoNaoEncontradoException {
+public class ProdutoNaoEncontradoException extends Exception {
+    public ProdutoNaoEncontradoException(String message) {
+        super(message);
+    }
 }
