2787518712058a3f3748ca59b22cd22c4ee17c4d
Matheus Vinícius <matheus678brdef@gmail.com>
2024-05-20 20:02:50 -0300

    Versão Final* da Classe Avaliação

--- src/main/java/com/example/projetopoobackendequipe4/repository/AvaliacaoRepository.java
+++ src/main/java/com/example/projetopoobackendequipe4/repository/AvaliacaoRepository.java
@@ -1,7 +1,5 @@
 package com.example.projetopoobackendequipe4.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,9 +8,5 @@ import com.example.projetopoobackendequipe4.model.Avaliacao;
 @Repository
 public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long>{
 
-    //List<Avaliacao> findByProduto(Produto produto);
-    //List<Avaliacao> findByEvento(Evento evento);
-    //List<Avaliacao> findByProdutor(Produtor produtor);
-    public double avaliacaoAlgoAvaliavel();
     public double mediaAvaliacoes();
 }
--- src/main/java/com/example/projetopoobackendequipe4/service/AvaliacaoService.java
+++ src/main/java/com/example/projetopoobackendequipe4/service/AvaliacaoService.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 import com.example.projetopoobackendequipe4.exception.AvaliacaoNaoEncontradaException;
 import com.example.projetopoobackendequipe4.model.Avaliacao;
-import com.example.projetopoobackendequipe4.model.Cliente;
 import com.example.projetopoobackendequipe4.repository.AvaliacaoRepository;
 
 @Service
@@ -56,26 +55,6 @@ public class AvaliacaoService {
 
     public List<Avaliacao> listaAvaliacoes() {
         return avaliacaoRepository.findAll();
-    }
-
-    /*public double avaliarProduto(Produto produto) {
-        List<Avaliacao> avaliacoes = avaliacaoRepository.findByProduto(produto);
-        return mediaAvaliacoes(avaliacoes);
-    }
-
-    public double avaliarEvento(Evento evento) {
-        List<Avaliacao> avaliacoes = avaliacaoRepository.findByEvento(evento);
-        return mediaAvaliacoes(avaliacoes);
-    }
-
-    public double avaliarProdutor(Produtor produtor) {
-        List<Avaliacao> avaliacoes = avaliacaoRepository.findByProdutor(produtor);
-        return mediaAvaliacoes(avaliacoes);
-    }*/
-
-    public double avaliacaoAlgoAvaliavel(Long id) {
-        List<Avaliacao> avaliacoes = avaliacaoRepository.findById(id);
-        return mediaAvaliacoes(avaliacoes);
     }
 
     public double mediaAvaliacoes(List<Avaliacao> avaliacoes) throws AvaliacaoNaoEncontradaException {
