2160e5dfbc14a6fc1647d391020bd92510e4d1f5
Ceciliasitcovsky <131723560+Ceciliasitcovsky@users.noreply.github.com>
2024-05-22 17:28:04 -0300

    Corrigindo ProdutoNaoEncontradoException.java

--- src/main/java/com/example/projetopoobackendequipe4/exception/ProdutoNaoEncontradoException.java
+++ src/main/java/com/example/projetopoobackendequipe4/exception/ProdutoNaoEncontradoException.java
@@ -1,18 +1,8 @@
+package com.example.projetopoobackendequipe4.exception;
 
-public Produto atualizarProduto(Long Id, Produto p) throws ProdutoNaoEncontradoException {
-        Optional<Produto> opProduto = produtoRepository.findById(Id);
-
-        if(opProduto.isEmpty()) {
-            throw new ProdutoNaoEncontradoException("Produto Não Encontrado");
-
-        }
-
-        Produto produto = opProduto.get();
-        produto.setNomeProduto(p.getNomeProduto());
-        produto.setDescricao(p.getDescricao());
-        produto.setCategoria(p.getCategoria());
-        produto.setEstoque(p.getEstoque());
-        produto.setFotoProduto(p.getFotoProduto());
-
-        return produtoRepository.save(produto);
+public class ProdutoNaoEncontradoException {
+public class ProdutoNaoEncontradoException extends Exception {
+    public ProdutoNaoEncontradoException(String message) {
+        super(message);
     }
+}
