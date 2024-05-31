d6d8ffad92700121e03fcc800db81fe5ce79ef53
Yuri Catunda <131727946+yuuricathugaa@users.noreply.github.com>
2024-05-31 14:48:52 -0300

    Update EventoController.java
    Atualizando o Evento Controller

--- src/main/java/com/example/projetopoobackendequipe4/controller/EventoController.java
+++ src/main/java/com/example/projetopoobackendequipe4/controller/EventoController.java
@@ -1,7 +1,9 @@
 package com.example.projetopoobackendequipe4.controller;
 
-import com.example.projetopoobackendequipe4.exception.EventoNaoEncontradoException;
+import com.example.projetopoobackendequipe4.exception.*;
+import com.example.projetopoobackendequipe4.model.Avaliacao;
 import com.example.projetopoobackendequipe4.model.Evento;
+import com.example.projetopoobackendequipe4.service.AvaliacaoService;
 import com.example.projetopoobackendequipe4.service.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,9 @@ public class EventoController {
     @Autowired
     private EventoService eventoService;
 
+    @Autowired
+    private AvaliacaoService avaliacaoService;
+
     @PostMapping("/criar")
     public ResponseEntity<Evento> criarEvento(@RequestBody Evento evento) {
         Evento novoEvento = eventoService.criarEvento(evento);
@@ -31,9 +36,9 @@ public class EventoController {
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Evento>> obterEventoPorId(@PathVariable Long id) {
+    public ResponseEntity<Evento> obterEventoPorId(@PathVariable Long id) {
         try {
-            Optional<Evento> evento = eventoService.obterEventoPorId(id);
+            Evento evento = eventoService.obterEventoPorId(id);
             return ResponseEntity.ok(evento);
         } catch (EventoNaoEncontradoException e) {
             return ResponseEntity.notFound().build();
@@ -56,6 +61,16 @@ public class EventoController {
             eventoService.deletarEvento(id);
             return ResponseEntity.noContent().build();
         } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/{id}/avaliacoes")
+    public ResponseEntity<List<Avaliacao>> listarAvaliacoesDeEvento(@PathVariable Long eventoId) {
+        try {
+            List<Avaliacao> avaliacoes = eventoService.listarAvaliacoesDeEvento(eventoId);
+            return ResponseEntity.ok(avaliacoes);
+        } catch (ProdutoNaoEncontradoException | AvaliacoesEventoNaoEncontradasException e) {
             return ResponseEntity.notFound().build();
         }
     }
